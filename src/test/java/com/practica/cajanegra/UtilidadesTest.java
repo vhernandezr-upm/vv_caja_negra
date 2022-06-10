38ca383c55b35d4c48ce62dac487128b6187b586
Víctor Hernández Rodríguez <v.hernandezr@alumnos.upm.es>
2022-06-10 15:06:20 +0200

    Añadidos test de la función reverse.

--- src/test/java/com/practica/cajanegra/UtilidadesTest.java
+++ src/test/java/com/practica/cajanegra/UtilidadesTest.java
@@ -2,6 +2,7 @@ package com.practica.cajanegra;
 
 import com.cajanegra.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -42,4 +43,30 @@ public class UtilidadesTest {
          assertTrue(lista.isEmpty());
     }
 
+     /* ####################################################################
+     *  ###                        TEST reverse                          ###
+     *  #################################################################### */
+
+    private static Stream<Arguments> reverse_reverseParams(){
+        List<Arguments> args = new LinkedList<>();
+        args.add(Arguments.of(new SingleLinkedListImpl<>('A', 'B', 'C', 'D'), new SingleLinkedListImpl<>('D', 'C', 'B', 'A')));
+        args.add(Arguments.of(new SingleLinkedListImpl<>('A', 'B', 'C'), new SingleLinkedListImpl<>('C', 'B', 'A')));
+        args.add(Arguments.of(new SingleLinkedListImpl<>('A', 'B'), new SingleLinkedListImpl<>('B', 'A')));
+        args.add(Arguments.of(new SingleLinkedListImpl<>('A'), new SingleLinkedListImpl<>('A')));
+        args.add(Arguments.of(new SingleLinkedListImpl<>(), new SingleLinkedListImpl<>()));
+
+        return args.stream();
+    }
+    @ParameterizedTest
+    @MethodSource("reverse_reverseParams")
+    public void reverse_reverseTest(SingleLinkedListImpl<Character> esperada, SingleLinkedListImpl<Character> lista){
+        assertIterableEquals(esperada, lista.reverse());
+    }
+
+    @Test
+    public void reverse_listaNoAlteradaTest(){
+        SingleLinkedListImpl<Character> listaInicial = lista;
+        lista.reverse();
+        assertIterableEquals(listaInicial, lista);
+    }
 }
